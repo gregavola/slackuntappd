4d3a2d6eda1c53ab5d9c81a1150f44d46094077c
Greg Avola <greg@untappd.com>
2014-08-31 10:13:42 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# slackuntappd - A Slackbot for Untappd
+
+This library is a simple script that allow you to enter a webhook URL and return beer information through the Slack interface by using the <code>untappd</code> command<br />
+
+<img src="http://f.cl.ly/items/2x0y3u3N260K0p2F1Q0Y/Screen%20Shot%202014-08-31%20at%2010.10.48%20AM.png" />
+
+# Requirements
+PHP 5+<br />
+CURL<br />
+
+# Getting Access to the Untappd API
+1. Head over to https://untappd.com/api and request and API key. Requests may take up to 2-3 weeks for approval, but once you get approval, add your <code>client_id</code> and <code>client_secret</code> to the <code>beer.php</code> file on line 26 and 27
+
+# Getting Started with Slack
+1. Upload <code>untappdPHP.php</code> and <code>beer.php</code> to your webserver.
+2. Log into your Slack Instance and click on the Title of the Slack Group and select Configure Integrations <img src="http://f.cl.ly/items/0V3l3b0Y363Z03292s16/open___ABC_News_Digital_Slack_and_New_Tab.png">
+3. Scroll down to the bottom of the page, and click "Outgoing Webhooks" <img src="http://f.cl.ly/items/2W153w0l3l0b1g0J003L/Add_Service_Integrations___ABC_News_Digital_Slack.png">
+4. Add all the details in to the form, including the URL to the endpoint you uploaded the files in the repo to <img src="http://f.cl.ly/items/3S2r0N0Y3f1C210u1X1q/Team_Integration___ABC_News_Digital_Slack.png" />
+5. Go into the channel you assigned the webhook and enter "untappd BEERNAME" and you'll be on your way!
+
+# To Do
+Error Handling
+
+# Getting Help
+If you need help or have questions, please contact Greg Avola on Twitter at http://twitter.com/gregavola
---
+++ beer.php
@@ -0,0 +1,87 @@
+<?php
+
+require ("untappdPHP.php");
+
+// Function for Vowels
+function checkifvowel($string) {
+		
+	$v = strtolower($string[0]);
+	
+	$beer_name = explode(" ", $string);
+	
+	if (sizeof($beer_name) > 1 && strtolower($beer_name[0]) == "the")
+	{
+		return "";
+	}
+	else if ($v == "a" || $v == "e" || $v == "i" || $v == "o" || $v == "u")
+	{
+		return "an";
+	}
+	else
+	{
+		return "a";
+	}
+}
+
+$client_id = "CLIENTIDHERE";
+$client_secret = "CLIENTSECRETHERE";
+$ut = new UntappdPHP($client_id, $client_secret, "");
+
+header("Content-type: application/json");
+
+
+$data = array();
+
+if (isset($_POST)) {
+
+	$event_json = json_decode(json_encode($_POST));
+
+	if (sizeof($event_json) == 0) {
+		$data["text"] = "Post is not empty, but has no results";
+
+	} else {
+		if (isset($event_json->text) && isset($event_json) && $event_json->trigger_word == "untappd") {
+			$beer_name = explode("untappd", $event_json->text);
+
+			$real_beer_name = trim($beer_name[1]);
+
+			$result = $ut->get("/search/beer", array("q" => $real_beer_name));
+
+			if ($result->meta->code == 200) {
+
+				if ($result->response->beers->count == 0) {
+					$data["text"] = "No results found for *".$real_beer_name."*";
+				} else {
+					$beer_id = $result->response->beers->items[0]->beer->bid;
+
+					$data["beer_id"] = $beer_id;
+
+					$result = $ut->get("/beer/info/".$beer_id);
+
+					if ($result->meta->code == 200) {
+						$beer = $result->response->beer;
+						$data["parse"] = "full";
+						$data["text"] = "*".$beer->beer_name . "* by *" . $beer->brewery->brewery_name . "* is ".checkifvowel($beer->beer_style)." ".$beer->beer_style ." at ".$beer->beer_abv."% with a rating of *" . round($beer->rating_score, 3) . "* - http://untappd.com/beer/".$beer->bid;
+					} else {
+						$data["text"] = $result->meta->code . " error on Beer Lookup - please try again! Error - ". $result->meta->error_detail;
+					}
+				}
+			} else {
+				$data["url"] = $url;
+				$data["text"] = $result->meta->code . " error on Beer Search - please try again! Error - ". $result->meta->error_detail;
+			}
+		}
+		else {
+			$v = json_encode($_POST);
+			$data["text"] = "Empty post response - " . $v;
+		}
+	}
+} else {
+	$data["text"] = "Post is empty";
+}
+
+
+echo json_encode($data);
+
+
+?>
